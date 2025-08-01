31047d189e1f7209fbe5089154e630f3d947b440
Shashi Kodiya <shashikodiya@gmail.com>
2025-08-01 20:52:10 +0530

    Day  1 Progress

---
+++ Day1_python.py
@@ -0,0 +1,12 @@
+#hello world
+print("Hello,Namaskara!")
+
+print("Hello My name is Shashank")
+
+Name="Shashank"
+
+print("hello"+Name)    #String Concatanation
+print("hello "+Name)   #String Concatanation
+print("hello {Name}")
+print(f"hello {Name}") #Formated String
+
