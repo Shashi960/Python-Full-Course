26b98cf64492673f0d560214b5c2d8967d7b5070
Shashi Kodiya <shashikodiya@gmail.com>
2025-08-11 00:56:59 +0530

    Loops

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,20 @@
-age=int(input("Enter Your Age: "))
-if(age>=18):
-    yes_no=input("you have voter id?(yes/no): ")
-    if(yes_no=="yes"):
-        print("You can Vote")
+h=int(input("Enter Hour: "))
+if(h<24):
+    t=int(input("Enter Time: "))
+    if(t>=60):
+        h=h+1
+        t=t-60
+    if(h<=12):
+        am_pm=input("AM or PM: ")
+        print(f"TIME:{h}:{t} {am_pm}")
     else:
-        print("You need to make voter card for voting")
-else:
-    print("You are not eligible to vote")
+        print(f"TIME:{h}:{t}")
+    if(h==8 and (am_pm=='AM' or am_pm=='am')):
+        print("It's Breakfast Time.")
+    elif((h==1 and (am_pm=='PM' or am_pm=='pm')) or h==13):
+        print("It's Dinner Time.")
+    elif((h==8 and (am_pm=='PM' or am_pm=='pm')) or h==20):
+        print("It's Lunch Time.")
+    else:
+        print("It's not meal time")
+        
