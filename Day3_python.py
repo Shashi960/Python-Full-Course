7889f5b00af33900f01ab80d979069713052909a
Shashi Kodiya <shashikodiya@gmail.com>
2025-08-10 20:59:09 +0530

    i/p,o/p,String And Escape Sequence

--- Day3_python.py
+++ Day3_python.py
@@ -62,5 +62,48 @@ print(line)
 line="hari is a \tgood boy"
 print(line)
 
+#Homework
+
+'''1.Simple Greeting Program:
+   Write a Python program that asks the user for their name and age, then prints a personalized greeting message. Use both the `+` operator and f-strings for output.'''
+name=input("Enter Your Name : ")
+age=input("Enter Your Age : ")
+print("Hello " + name + "! You are " + age + " years old.")  # Using + operator
+print(f"Hello {name}! You are {age} years old.")  # Using f-string
 
 
+'''2.String Manipulation Exercise:
+   Write a Python program that:
+   - Takes a sentence as input from the user.
+   - Prints the sentence in all uppercase and lowercase.
+   - Replaces all spaces with underscores.
+   - Removes leading and trailing whitespace'''
+line=input("Enter a Sentence: ")
+print(f"Uppercase :{line.upper()}")
+print(f"Uppercase :{line.lower()}")
+print(f"Uppercase :{line.replace(" ","_")}")
+print(f"Uppercase :{line.strip()}")
+
+'''3.Character Counter
+   Write a Python program that:
+   - Asks the user for a string.
+   - Prints how many characters are in the string, excluding spaces.
+'''
+str=input("Enter a String: ")
+i=0
+for char in str:
+    if char!=" ":
+        i=i+1
+print(f"Total number of character in a given string is: {i} ")
+
+'''4.Escape Sequence Practice:
+   Write a Python program that uses escape sequences to print the following output:
+Output:
+   ```
+   Hello
+       World
+   This is a backslash: \
+   ```
+'''
+str="Hello \n\t World\n This is a backslash: \\"
+print(str)
