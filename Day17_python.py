810bf4dc1ae17ff14861208a162a1a3c61b44c12
Shashi Kodiya <shashikodiya@gmail.com>
2025-08-16 20:43:35 +0530

    OOP Pillars

--- Day17_python.py
+++ Day17_python.py
@@ -56,6 +56,45 @@ db = Database()
 db.save_data("user_101", {"name": "Raj", "age": 30})
 print(db.get_data("user_101"))
 
+#Inheritance
+class User:
+    def __init__(self, username):
+        self.username = username
+
+    def login(self):
+        print(f"{self.username} logged in")
+
+class Admin(User):
+    def delete_user(self, user):
+        print(f"Admin {self.username} deleted user {user}")
+
+admin = Admin("karnataka_admin")
+admin.login()  # Inherited from User
+admin.delete_user("user_102")
+
+
+#Polymorphism
+class User:
+    def __init__(self, username):
+        self.username = username
+
+    def login(self):
+        print(f"{self.username} logged in")
+
+class Admin(User):
+    def delete_user(self, user):
+        print(f"Admin {self.username} deleted user {user}")
+
+admin = Admin("karnataka_admin")
+admin.login()  # Inherited from User
+admin.delete_user("user_102")
+
+
+
+
+
+
+
 
 #Homework
 '''1.Encapsulation:
