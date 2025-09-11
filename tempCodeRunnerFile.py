74fcfe5707f8f25305716d4b779a0d36cb9aa7a3
Shashi Kodiya <shashikodiya@gmail.com>
2025-09-11 22:58:37 +0530

    File Handling(Day23_python.py)  operation files

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,27 +1,7 @@
-import math
-class Shape:
-    def calculate_area(self):
-        pass 
-class Circle(Shape):
-    def __init__(self, radius):
-        self.radius = radius
-
-    def calculate_area(self):
-        return math.pi * (self.radius ** 2)
-    
-class Rectangle(Shape):
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-    
-    def calculate_area(self):
-        return self.width * self.height
-
-shapes = [
-    Circle(5),
-    Rectangle(4, 6)
-]
-
-for shape in shapes:
-    area = shape.calculate_area()
-    print(f"Area: {area:.2f}")
+with open('friends.txt','r') as file:
+    content=file.read()
+    name=input("Enter search Name in friends.txt: ")
+    if name in content:
+        print("Found!")
+    else:
+        print("Not found")
